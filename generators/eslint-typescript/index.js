b967d44e637ac280aee20faf261993abad64dc24
AllegriM <elmonky99@gmail.com>
2023-04-21 15:40:55 -0300

    feat: :tada: Begin project!

---
+++ generators/eslint-typescript/index.js
@@ -0,0 +1,71 @@
+const Generator = require('yeoman-generator');
+
+// 
+module.exports = class extends Generator {
+    installDependencies() {
+        this.spawnCommandSync("pnpm", [
+            "add",
+            "-D",
+            "prettier",
+            "eslint",
+            "eslint-config-prettier",
+            "eslint-config-standard",
+            "eslint-plugin-import",
+            "eslint-plugin-node",
+            "eslint-plugin-n",
+            "eslint-plugin-prettier",
+            "eslint-plugin-promise",
+            "eslint-plugin-react",
+            "eslint-plugin-react-hooks",
+            "tailwindcss",
+            "postcss",
+            "autoprefixer",
+            "@typescript-eslint/parser",
+            "@typescript-eslint/eslint-plugin",
+        ]);
+    }
+
+    // Copy files
+
+    eslint() {
+        this.fs.copy(
+            this.templatePath('.eslintrc.cjs'),
+            this.destinationPath('.eslintrc.cjs')
+        );
+    }
+    vscode() {
+        this.fs.copy(
+            this.templatePath('.vscode'),
+            this.destinationPath('.vscode')
+        );
+    }
+    eslintIgnore() {
+        this.fs.copy(
+            this.templatePath('.eslintignore'),
+            this.destinationPath('.eslintignore')
+        );
+    }
+    editorconfig() {
+        this.fs.copy(
+            this.templatePath('.editorconfig'),
+            this.destinationPath('.editorconfig')
+        );
+    };
+
+    // Create files to destination folder
+
+    tailwind() {
+        this.fs.copy(
+            this.templatePath('tailwind.config.js'),
+            this.destinationPath('tailwind.config.js')
+        );
+    }
+
+    postcss() {
+        this.fs.copy(
+            this.templatePath('postcss.config.js'),
+            this.destinationPath('postcss.config.js')
+        );
+    }
+
+}
