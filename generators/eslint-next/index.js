2c7486aa220cdfa9afd09f0f0bf9580ab4bd1279
AllegriM <elmonky99@gmail.com>
2023-04-21 17:11:25 -0300

    feat: :sparkles: add vercel-eslint config

---
+++ generators/eslint-next/index.js
@@ -0,0 +1,56 @@
+const Generator = require('yeoman-generator');
+
+// 
+module.exports = class extends Generator {
+    installDependencies() {
+        this.spawnCommandSync("pnpm", [
+            "add",
+            "-D",
+            "eslint",
+            "tailwindcss",
+            "postcss",
+            "autoprefixer",
+            "eslint-config-prettier",
+            "eslint-plugin-prettier",
+            "@typescript-eslint/parser",
+            "@typescript-eslint/eslint-plugin",
+            "eslint-import-resolver-typescript",
+            "eslint-plugin-import",
+            "@vercel/style-guide",
+        ]);
+    }
+
+    eslint() {
+        this.fs.copy(this.templatePath(".eslintrc.cjs"), this.destinationPath(".eslintrc.cjs"));
+        this.fs.delete(this.destinationPath(".eslintrc.json"));
+    }
+
+    vscode() {
+        this.fs.copy(this.templatePath(".vscode"), this.destinationPath(".vscode"));
+    }
+
+    eslintIgnore() {
+        this.fs.copy(this.templatePath(".eslintignore"), this.destinationPath(".eslintignore"));
+    }
+
+    editorconfig() {
+        this.fs.copy(this.templatePath(".editorconfig"), this.destinationPath(".editorconfig"));
+    }
+
+    // Create files to destination folder
+
+    tailwind() {
+        this.fs.copy(
+            this.templatePath('tailwind.config.js'),
+            this.destinationPath('tailwind.config.js')
+        );
+    }
+
+    postcss() {
+        this.fs.copy(
+            this.templatePath('postcss.config.js'),
+            this.destinationPath('postcss.config.js')
+        );
+    }
+
+}
